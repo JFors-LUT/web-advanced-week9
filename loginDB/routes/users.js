45b1cf35fa2ca1eb2dea0b69a2d3d68f9e0e2641
Jarno <jarno.forsblom@student.lut.fi>
2023-07-26 22:51:13 +0300

    text_commit

--- loginDB/routes/users.js
+++ loginDB/routes/users.js
@@ -3,73 +3,64 @@ const bcrypt = require('bcryptjs');
 const { v4: uuidv4 } = require('uuid');
 const jwt = require('jsonwebtoken');
 const mongoose = require("mongoose");
+const passport = require('passport');
+const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
+
 const User = require('../models/Users');
 var router = express.Router();
 
 const SECRET = process.env.SECRET || 'secret';
 
+//configure for passport
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: SECRET,
+};
+
+passport.use(
+  new JwtStrategy(jwtOptions, async (payload, done) => {
+    try {
+      const user = await User.findOne({ username: payload.username });
+      if (!user) {
+        return done(null, false);
+      }
+
+      return done(null, user);
+    } catch (error) {
+      return done(error, false);
+    }
+  })
+);
 
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-/*
-router.post('/api/user/register', async (req, res) => {
-  const { username, password } = req.body;
-  console.log(req.body);
-  const userFound = await userModel.findUserByUsername(username)
-  if (userFound) {
-    return res.status(403).json({email:'Email already in use.'});
-  }
-
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const userId = uuidv4();
-
-  const newUser = { username, password: hashedPassword };
-  userModel.addUser(newUser);
-
-  res.status(201).json('User registered');
+router.get('/api/private', passport.authenticate('jwt', { session: false }), (req, res) => {
+  console.log(req.body)
+  return res.status(200).json({ username: req.user.username });
 });
-*/
 
-/*
-router.post('/api/user/register', (req, res, next) => {
-  
-  const user = req.body.username;
-  const password  = req.body.password;
+const strongPasswordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[~`!@#$%^&*()-_+={}[\]|\\;:"<>,./?]).{8,}$/;
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-  User.findOne({ username:user }) 
-    .exec()
-    .then((userFind) => {
-      if (!userFind) {
-        return User.create({
-          username: user,
-          password: password,
-        });
-      } else {
-        return Promise.reject('User already in database.');
-      }
-    })
-    .then((user) => {
-      res.status(201).json(user);
-    })
-    .catch((error) => {
-      if (error === 'User already in database.') {
-        return res.status(403).json({email:'Email already in use.'});;
-      } else {
-        return res.status(500).json({error});
-      }
-    });
-});
-*/
-router.post('/api/user/register', async (req, res) => {
-  const { username, password } = req.body;
+router.post('/api/user/register', async (req, res, next) => {
+  const username = req.body.username; 
+  const password = req.body.password; 
 
   try {
     const userExists = await User.findOne({ username });
 
     if (userExists) {
       return res.status(403).json('Username already in use.');
+    }
+
+    if (!emailRegex.test(username)) {
+      return res.status(400).json('Invalid email format.');
+    }
+
+    if (!strongPasswordRegex.test(password)) {
+      return res.status(400).json('Invalid password format. Password should include at least one lowercase letter, one uppercase letter, one number, one symbol and 8 characters');
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -79,9 +70,9 @@ router.post('/api/user/register', async (req, res) => {
       password: hashedPassword,
     });
 
-   await newUser.save();
+    await newUser.save();
 
-    res.status(201).json("ok");
+    res.status(201).json('ok');
   } catch (error) {
     console.error(error);
     return res.status(500).json('error');
@@ -97,19 +88,16 @@ router.post('/api/user/login', async (req, res) => {
 
   try {
     const user = await User.findOne({ username });
-    console.log(user)
     if (!user) {
       return res.status(401).json('Invalid username');
     };
 
     const passwordMatch = await bcrypt.compare(password, user.password);
-    console.log(passwordMatch)
     if (!passwordMatch) {
       return res.status(401).json('Invalid credentials');
     };
-
     const payload = {
-      email: user.email,
+      username: user.username,
     };
 
     jwt.sign(payload, SECRET, (err, token) => {
@@ -125,44 +113,5 @@ router.post('/api/user/login', async (req, res) => {
     return res.status(500).json(error);
   }
 });
-
-
-/*
-router.post('/api/user/login', async (req, res) => {
-  const { username, password } = req.body;
-  const user = await userModel.findUserByUsername(username);
-
-  if (!user) {
-    return res.status(401).json('Invalid credentials');
-  }
-
-  const passwordMatch = await bcrypt.compare(password, user.password);
-
-  if (!passwordMatch) {
-    return res.status(401).json('Invalid credentials');
-  }
-
-  const token = jwt.sign({ username: user.username }, 'secret');
-  res.status(200).json({ token });
-});
-
-  const verifyToken = (req, res, next) => {
-    const token = req.headers.authorization?.split(' ')[1];
-  
-    if (!token) {
-      return res.status(401).json('Unauthorized');
-    }
-  
-    jwt.verify(token, 'secret', (err, decodedToken) => {
-      if (err) {
-        return res.status(401).json('Invalid token');
-      }
-  
-      req.userId = decodedToken.userId;
-      next();
-    });
-  };
-*/
-
 
 module.exports = router;
