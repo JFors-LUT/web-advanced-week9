26ae89d367040418f5a25cf493f6e00865e4bddf
Jarno <jarno.forsblom@student.lut.fi>
2023-07-26 21:03:33 +0300

    ifthis

--- loginDB/models/Users.js
+++ loginDB/models/Users.js
@@ -1,16 +1,10 @@
 const mongoose = require("mongoose");
 
+const Schema = mongoose.Schema
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
+let userSchema = new Schema({
+  username: String,
+  password: String,
 });
 
 const User = mongoose.model('User', userSchema);
